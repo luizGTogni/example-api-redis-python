ffd375483dbecdb3c4f7a252004fe2df1a973c3e
Luiz Togni <contato.togni@gmail.com>
2025-08-09 19:55:58 -0300

    feat: errors types

---
+++ src/types/errors/http_not_found.py
@@ -0,0 +1,5 @@
+from .http_error import HttpError
+
+class HttpNotFoundError(HttpError):
+    def __init__(self, message: str) -> None:
+        super().__init__(status_code=404, name="NotFound", message=message)
